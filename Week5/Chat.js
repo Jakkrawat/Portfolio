2fd674831878a631c73510444e749092f27e28dc
Jakkrawat <32680348+Jakkrawat@users.noreply.github.com>
2021-03-25 03:03:45 +0700

    Add files via upload

---
+++ Week5/Chat.js
@@ -0,0 +1,41 @@
+var topic1 = document.getElementById("Topic1");
+var comment1 = document.getElementById("Comment1");
+var comment2 = document.getElementById("Comment2");
+
+topic1 = 0;
+comment1 = 0;
+comment2 = 0;
+
+alert("อยากเกิดก็เปิดแชทดิ");
+function postFunction() { 
+    
+    var text1 = document.getElementById("Text1").value
+    if (topic1 == 0) {
+        document.getElementById("Topic1").innerText = text1
+        topic1 = text1
+        alert("รอคนมาเม้นอยู่นะ");
+    }
+    else if (comment1 == 0) {
+        document.getElementById("Comment1").innerText = text1
+        comment1 = text1
+       alert("ขออีกคนๆ");
+    }
+    else if (comment2 == 0) {
+        document.getElementById("Comment2").innerText = text1
+        comment2 = text1
+        alert("พอละ ไม่ต้องเม้นละนะ");
+    }
+}
+
+function clearFunction() { 
+    alert("ล้างกระดาน!!!");
+    topic1 = 0;
+    comment1 = 0;
+    comment2 = 0;
+    document.getElementById("Topic1").innerText = "";
+    document.getElementById("Comment1").innerText = "";
+    document.getElementById("Comment2").innerText = "";
+    document.getElementById("Text1").value = "";
+}
+
+
